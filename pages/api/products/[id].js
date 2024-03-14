6a260bca8f8314aa41821c613d86f12a926beb8c
Andrea Hohnsen <a.hohnsen@mail.de>
2024-03-14 16:26:01 +0100

    Implement api routes for PUT and DELETE

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -14,4 +14,18 @@ export default async function handler(request, response) {
 
     response.status(200).json(product);
   }
+
+  if (request.method === "PUT") {
+    const updatedProduct = request.body;
+
+    await Product.findByIdAndUpdate(id, updatedProduct);
+
+    response.status(200).json({ status: "Product successfully updated." });
+  }
+
+  if (request.method === "DELETE") {
+    await Product.findByIdAndDelete(id);
+
+    response.status(200).json({ status: "Product successfully deleted." });
+  }
 }
